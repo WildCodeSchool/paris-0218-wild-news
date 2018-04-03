a42b5c3f9421d403ba008af72d2af8ead39f1e60
Kraamel <b.amel.braiek@gmail.com>
2018-04-03 18:36:22 +0200

    correction js

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-const express = require("express")
-const post1 = require("../mocks/posts/post1.json")
+const express = require('express')
+const post1 = require('../mocks/posts/post1.json')
 
 const posts = post1
 const app = express()
 
 app.use((request, response, next) => {
-  response.header("Access-Control-Allow-Origin", "*")
+  response.header('Access-Control-Allow-Origin', '*')
   response.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   )
   next()
 })
 
-app.get("/", (request, response) => {
-  response.send("OK")
+app.get('/', (request, response) => {
+  response.send('OK')
 })
 
-app.get("/post", (request, response) => {
+app.get('/post', (request, response) => {
   response.json(posts)
 })
 
