1f201efafc664dfd7e5b74755d97ee393b65a015
Guillaume Sibon <guillaume.infonews@gmail.com>
2018-04-11 10:55:08 +0200

    page js pour post

---
+++ client/js/app-posts.js
@@ -0,0 +1,13 @@
+import {createPostElement} from './components/home.js' // ce module permet de créer les posts qui sont dans les containers category de la page home
+
+fetch('http://localhost:3000/posts/javaScript/post1.json')
+  .then(response => response.json())
+  .then(posts => {
+    console.log(posts) // ce console.log sert juste a verifier dans la console si tout fonctionne, il n'est donc pas obligatoire
+    Object.keys(posts).forEach(function (category) { // object.keys est utilisé ici car le forEach ne fonctionne QUE sur des arrays, hors on a un objet donc en passant par les clés on peut réaliser un forEach. Voir plus sur MDN https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+      const categoriesContainer = document.querySelector('.categories') // ce premier constituera les containers des catégories
+      categoriesContainer.innerHTML += createCategoryElement(category)
+      const postsContainer = document.querySelector('.cat-' + category.toLowerCase()) // celui la complètera avec les articles correspondant
+      postsContainer.innerHTML = posts[category].map(createPostElement).join('')
+    })
+  })
