a851069ea8f6ad3de0e4a9dddc2ab4f962658557
Guillaume Sibon <guillaume.infonews@gmail.com>
2018-04-13 19:18:44 +0200

    ajout fichier category.js retrait majuscule mocks

--- client/js/app.js
+++ client/js/app.js
@@ -4,11 +4,11 @@ fetch('http://localhost:3000/post')
   .then(response => response.json())
   .then(posts => {
     const postsElementTips = document.getElementById('posts-tips')
-    const postsCategory1 = posts.filter(post => post.category === 'Tips')
+    const postsCategory1 = posts.filter(post => post.category === 'tips')
     const postElementTips = postsCategory1.map(createNewPost).join('')
 
     const postsElementJavascript = document.getElementById('posts-javascript')
-    const postsCategory2 = posts.filter(post => post.category === 'Javascript')
+    const postsCategory2 = posts.filter(post => post.category === 'javascript')
     const postElementJavascript = postsCategory2.map(createNewPost).join('')
 
     postsElementTips.innerHTML = postElementTips
--- client/js/category.js
+++ client/js/category.js
@@ -1,11 +1,12 @@
 import { createNewPost } from '/components/block-category.js'
 
-fetch('http://localhost:3000/post')
+const params = new URLSearchParams(window.location.search)
+const name = params.get('name')
+fetch(`http://localhost:3000/category/${name}`)
   .then(response => response.json())
   .then(posts => {
     const postsElementTips = document.getElementById('posts-tips')
-    const postsCategory1 = posts.filter(post => post.category === 'Tips')
+    const postsCategory1 = posts.filter(post => post.category === `${name}`)
     const postElementTips = postsCategory1.map(createNewPost).join('')
-
     postsElementTips.innerHTML = postElementTips
   })
