8dd02226af142bf88eb9b6acb705c98f961234e5
Lor Chtg <lorraine.chastang@gmail.com>
2018-04-13 14:56:42 +0200

    change html js and css files and add tips category image

--- client/components/block-article-accueil.js
+++ client/components/block-article-accueil.js
@@ -1,8 +1,8 @@
 export
 const createNewPost = (monPost) =>
-` <div class="article-caption row">
-     <a href="/post.html?id=${monPost.id}">
-     <div id="image"><img src="${monPost.image}" alt="${monPost.title}"></div>
+` <div class="row article-caption">
+     <a href="/post.html?id=${monPost.id}" class="article-link">
+     <div id="image" class="column column-25"><img src="${monPost.image}" alt="${monPost.title}" class="article-img"></div>
      <div class="column column-75">
        <h4 id="posts">${monPost.title}</h4>
        <p id="description">${monPost.text}</p>
