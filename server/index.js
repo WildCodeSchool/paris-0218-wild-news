4bfe729681d50a62bfb7f8480d125fa12b73af1e
Guillaume Sibon <guillaume.infonews@gmail.com>
2018-04-21 12:38:58 +0200

    clean code and add comment

--- server/index.js
+++ server/index.js
@@ -1,23 +1,24 @@
+//==============MODULES==============//
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
-const util = require('util')
-const readFile = util.promisify(fs.readFile)
+const util = require('util')                          //  Filesystem
+const readFile = util.promisify(fs.readFile)          //  Util. pour les chemins d'accès
 const readdir = util.promisify(fs.readdir)
 const writeFile = util.promisify(fs.writeFile)
-
 const app = express()
 
+
+//==============HEADER==============//
 app.use((request, response, next) => {
   console.log('je set les headers cors')
   response.header('Access-Control-Allow-Origin', '*')
-  response.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
 
+
+//==============ACCUMULATOR==============//
 app.use((request, response, next) => {
   console.log('je parse le body')
   if (request.method === 'GET') return next()
@@ -38,19 +39,17 @@ app.use((request, response, next) => {
 })
 
 
+//==============HOME==============//
 app.get('/', (request, response) => {
   response.send('OK')
 })
 
 
-
-
-
-// ****************ROUTE POUR ACCUEIL RETOURNE TOUS LES POSTS******************//
-
+//==============GET ALL POSTS==============//
 app.get('/post', (request, response) => {
 
   const postsDir = path.join(__dirname,'../', 'mocks/posts') // construit URL : path ajoute des / et join réunit tout
+  console.log(postsDir)
 
   readdir(postsDir) // récupère tous les noms de fichiers mais seulement les noms pas chemin complet , en l'occurence post1.json
     .then(files => Promise.all(files
@@ -64,9 +63,39 @@ app.get('/post', (request, response) => {
 })
 
 
+//==============GET CATEGORY BY ID==============//
+app.get('/category/:name', (request, response) => {
 
-// ****************ROUTE POUR POST SUR ACCUEIL******************//
+  const postsDir = path.join(__dirname,'../', 'mocks/posts') // construit URL : path ajoute des / et join réunit tout
 
+  readdir(postsDir) // récupère tous les noms de fichiers mais seulement les noms pas chemin complet , en l'occurence post1.json
+    .then(files => Promise.all(files //prendre un tableau de promesses et le convertir en tableau de valeur des promesses
+      .map(file => path.join(postsDir, file))
+      .map(filepath => readFile(filepath, 'utf8'))))
+
+
+    .then(allFilesValues => response.json(allFilesValues.map(JSON.parse)))
+    .catch(err => response.status(500).end(err.message))
+
+})
+
+//=============GET POST BY ID==============//
+app.get('/post/:id', (request, response) => {
+  const fileName = `post${request.params.id}.json`
+  const filepath = path.join(__dirname,'../', 'mocks/posts', fileName)
+
+  readFile(filepath)
+    .then(data => {
+      response.header('Content-Type', 'application/json; charset=utf-8')
+      response.end(data)
+    })
+    .catch(err=> {
+      response.status(404).end('Post introuvable')
+    })
+})
+
+
+//==============POST NEW POST==============//
 app.post('/post', (request, response, next) => {
 
   console.log('ok')
@@ -92,8 +121,7 @@ app.post('/post', (request, response, next) => {
 })
 
 
-// ****************ROUTE POUR ADD CATEGORIE******************//
-
+//==============POST NEW CATEGORY==============//
 app.post('/category', (request, response, next) => {
   const id = Math.random().toString(36).slice(2).padEnd(11, '0')
   const filename = `${id}.json`
@@ -112,54 +140,17 @@ app.post('/category', (request, response, next) => {
 })
 
 
-
-// ****************ROUTE POUR LES CATEGORY AVEC PARAMETRE******************//
-
-app.get('/category/:name', (request, response) => {
-
-  const postsDir = path.join(__dirname,'../', 'mocks/posts') // construit URL : path ajoute des / et join réunit tout
-
-  readdir(postsDir) // récupère tous les noms de fichiers mais seulement les noms pas chemin complet , en l'occurence post1.json
-    .then(files => Promise.all(files //prendre un tableau de promesses et le convertir en tableau de valeur des promesses
-      .map(file => path.join(postsDir, file))
-      .map(filepath => readFile(filepath, 'utf8'))))
-
-
-    .then(allFilesValues => response.json(allFilesValues.map(JSON.parse)))
-    .catch(err => response.status(500).end(err.message))
-
-})
-
-
-
-// ***ROUTE POUR LES PAGES POST INDIVIDUELS RETOURNE LES POSTS AVEC LE MEME PARAMETRE**//
-
-app.get('/post/:id', (request, response) => {
-  const fileName = `post${request.params.id}.json`
-  const filepath = path.join(__dirname,'../', 'mocks/posts', fileName)
-
-  readFile(filepath)
-    .then(data => {
-      response.header('Content-Type', 'application/json; charset=utf-8')
-      response.end(data)
-    })
-    .catch(err=> {
-      response.status(404).end('Post introuvable')
-    })
-
-})
-
-// ****************ROUTE POUR LA NAVBAR****************** //
-
+//==============GET NAV BAR==============//
 app.get('/navbar', (request, response) => {
 
   const navBarDir = path.join(__dirname,'../', 'mocks/category') // construit URL : path ajoute des / et join réunit tout
-
+  console.log(navBarDir)
   readdir(navBarDir) // récupère tous les noms de fichiers mais seulement les noms pas chemin complet , en l'occurence post1.json
     .then(files => {
       const filepaths = files.map(file => path.join(navBarDir, file))
       const allFiles = filepaths.map(filepath => {
         return readFile(filepath, 'utf8')
+        console.log(readFile)
       })
 
         Promise.all(allFiles)
@@ -185,9 +176,5 @@ app.get('/navbar', (request, response) => {
     })
 })
 
-
-
-
+//==============PORT==============//
 app.listen(3000, () => console.log("j'écoute sur le port 3000"))
-
-
