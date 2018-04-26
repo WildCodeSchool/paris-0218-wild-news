4a3b5ff40db61fe5e9af08601938c8cbb88eb09f
Guillaume Sibon <guillaume.infonews@gmail.com>
2018-04-26 12:45:40 +0200

    export explicit functions

--- server/db.js
+++ server/db.js
@@ -25,5 +25,14 @@ const createUser = params => exec(`
   INSERT INTO user (username, firstName, lastName, email, password)
   VALUES (:username, :firstName, :lastName, :email, :password)`, params)
 const readUser = () => exec(`SELECT * FROM user`)
-readUser.byId = id => exec(`SELECT * FROM user WHERE id=:id`, { id })
-readUser.by = params => exec(`SELECT * FROM user WHERE ${where(params)}`, params)
+const readUserById = id => exec(`SELECT * FROM user WHERE id=:id`, { id })
+const readUserby = params => exec(`SELECT * FROM user WHERE ${where(params)}`, params)
+
+module.exports = {
+  user: {
+    create: createUser,
+    read: readUser,
+    readBy: readUserby,
+    readById: readUserById
+  }
+}
